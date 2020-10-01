04d4cfc10afdd48bb89e199799176a8bf139aa09
Riccardo Binetti <riccardo.binetti@ispirata.com>
2020-10-02 01:36:00 +0200

    main: take options from the command line
    Signed-off-by: Riccardo Binetti <riccardo.binetti@ispirata.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
 use astarte_sdk::DeviceBuilder;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    // Realm name
+    #[structopt(short, long)]
+    realm: String,
+    // Device id
+    #[structopt(short, long)]
+    device_id: String,
+    // Credentials secret
+    #[structopt(short, long)]
+    credentials_secret: String,
+    // Pairing URL
+    #[structopt(short, long)]
+    pairing_url: String,
+}
 
 fn main() {
-    let realm = "rbino";
-    let device_id = "le5YqVm6TdORty9NqYD8Yw";
-    let credentials_secret = "foobar";
-    let pairing_url = "https://api.astarte.example.com/pairing";
+    let Cli {
+        realm,
+        device_id,
+        credentials_secret,
+        pairing_url,
+    } = Cli::from_args();
 
-    let d = DeviceBuilder::new(&realm, &device_id)
-        .credentials_secret(credentials_secret)
-        .pairing_url(pairing_url)
+    let mut d = DeviceBuilder::new(&realm, &device_id)
+        .credentials_secret(&credentials_secret)
+        .pairing_url(&pairing_url)
         .build()
         .unwrap();
 
-    println!("{:?}", d);
+    if let Err(e) = d.obtain_credentials() {
+        println!("Error: {:?}", e);
+        return;
+    }
+
+    println!("Device: {:?}", d);
 }
