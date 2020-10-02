2a098053071e673d7fdde4d67b02c93c39c17e6f
Riccardo Binetti <riccardo.binetti@ispirata.com>
2020-10-02 13:22:59 +0200

    pairing: add fetch_broker_url
    Restructure code to decode API responses
    
    Signed-off-by: Riccardo Binetti <riccardo.binetti@ispirata.com>

--- src/pairing.rs
+++ src/pairing.rs
@@ -7,13 +7,31 @@ use serde_json::json;
 use url::ParseError;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct AstarteMQTTV1Credentials {
-    client_crt: String,
+struct ApiResponse {
+    data: ResponseContents,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct CredentialsResponse {
-    data: AstarteMQTTV1Credentials,
+#[serde(untagged)]
+enum ResponseContents {
+    AstarteMqttV1Credentials {
+        client_crt: String,
+    },
+    StatusInfo {
+        version: String,
+        status: String,
+        protocols: ProtocolsInfo,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProtocolsInfo {
+    astarte_mqtt_v1: AstarteMqttV1Info,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AstarteMqttV1Info {
+    broker_url: String,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -24,6 +42,8 @@ pub enum PairingError {
     InvalidUrl(#[from] ParseError),
     #[error("error while sending or receiving request")]
     RequestError(#[from] reqwest::Error),
+    #[error("API response can't be deserialized")]
+    UnexpectedResponse,
     #[error("API returned an error code")]
     ApiError(StatusCode, String),
     #[error("crypto error")]
@@ -72,12 +92,62 @@ pub async fn fetch_credentials(device: &Device) -> Result<String, PairingError> 
 
     match response.status() {
         StatusCode::CREATED => {
-            let certificate = response
-                .json::<CredentialsResponse>()
-                .await?
-                .data
-                .client_crt;
-            Ok(String::from(certificate))
+            if let ResponseContents::AstarteMqttV1Credentials { client_crt } =
+                response.json::<ApiResponse>().await?.data
+            {
+                Ok(String::from(client_crt))
+            } else {
+                Err(PairingError::UnexpectedResponse)
+            }
+        }
+
+        status_code => {
+            let raw_response = response.text().await?;
+            Err(PairingError::ApiError(status_code, raw_response))
+        }
+    }
+}
+
+pub async fn fetch_broker_url(device: &Device) -> Result<String, PairingError> {
+    let Device {
+        realm,
+        device_id,
+        credentials_secret,
+        pairing_url,
+        ..
+    } = device;
+
+    let mut url = Url::parse(&pairing_url)?;
+    // We have to do this this way to avoid unconsistent behaviour depending
+    // on the user putting the trailing slash or not
+    url.path_segments_mut()
+        .map_err(|_| ParseError::RelativeUrlWithCannotBeABaseBase)?
+        .push("v1")
+        .push(&realm)
+        .push("devices")
+        .push(&device_id);
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .bearer_auth(&credentials_secret)
+        .send()
+        .await?;
+
+    match response.status() {
+        StatusCode::OK => {
+            if let ResponseContents::StatusInfo {
+                protocols:
+                    ProtocolsInfo {
+                        astarte_mqtt_v1: AstarteMqttV1Info { broker_url },
+                    },
+                ..
+            } = response.json::<ApiResponse>().await?.data
+            {
+                Ok(String::from(broker_url))
+            } else {
+                Err(PairingError::UnexpectedResponse)
+            }
         }
 
         status_code => {
