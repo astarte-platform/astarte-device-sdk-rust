227ec8bcd87860a7fd3c32cfca37336b8c96afcb
shikoku1 <87471904+shikoku1@users.noreply.github.com>
2021-10-06 13:32:35 +0200

    Add tests
    Signed-off-by: shikoku1 <87471904+shikoku1@users.noreply.github.com>
    Signed-off-by: shikoku1 <gia.zala@secomind.com>

--- src/lib.rs
+++ src/lib.rs
@@ -460,11 +460,8 @@ impl AstarteSdk {
                             }
 
                             if cfg!(debug_assertions) {
-                                self.interfaces.validate_receive(
-                                    &interface,
-                                    &path,
-                                    bdata.clone(),
-                                )?;
+                                self.interfaces
+                                    .validate_receive(&interface, &path, &bdata)?;
                             }
 
                             let data = AstarteSdk::deserialize(&bdata)?;
