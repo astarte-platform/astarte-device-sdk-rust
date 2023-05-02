f0ec3fcaedec3c39d0190d16d3aad48061db0f24
Simone Orru <simone.orru@secomind.com>
2023-05-02 09:36:00 +0200

    Adds attribute for struct renaming
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

---
+++ astarte-device-sdk-derive/src/case.rs
@@ -0,0 +1,165 @@
+/*
+ * This file uses source code from the files /serde_derive/src/internals/case.rs, from
+ * https://github.com/serde-rs/serde, copyright Erick Tryzelaar and David Tolnay, licensed under the
+ * Apache 2.0 license.
+ *
+ * This file is part of Astarte.
+ *
+ * Copyright 2023 SECO Mind Srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+use std::fmt::{self, Debug, Display};
+
+/// The different possible ways to change case of fields in a struct.
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum RenameRule {
+    /// Do not rename.
+    None,
+    /// Rename to "lowercase" style.
+    LowerCase,
+    /// Rename to "UPPERCASE" style.
+    UpperCase,
+    /// Rename to "PascalCase" style.
+    PascalCase,
+    /// Rename to "camelCase" style.
+    CamelCase,
+    /// Rename to "snake_case" style.
+    SnakeCase,
+    /// Rename to "SCREAMING_SNAKE_CASE" style.
+    ScreamingSnakeCase,
+    /// Rename to "kebab-case" style.
+    KebabCase,
+    /// Rename to "SCREAMING-KEBAB-CASE" style.
+    ScreamingKebabCase,
+}
+
+static RENAME_RULES: &[(&str, RenameRule)] = &[
+    ("lowercase", RenameRule::LowerCase),
+    ("UPPERCASE", RenameRule::UpperCase),
+    ("PascalCase", RenameRule::PascalCase),
+    ("camelCase", RenameRule::CamelCase),
+    ("snake_case", RenameRule::SnakeCase),
+    ("SCREAMING_SNAKE_CASE", RenameRule::ScreamingSnakeCase),
+    ("kebab-case", RenameRule::KebabCase),
+    ("SCREAMING-KEBAB-CASE", RenameRule::ScreamingKebabCase),
+];
+
+impl RenameRule {
+    /// Obrain a rename rule from a str
+    pub fn from_str(rename_all_str: &str) -> Result<Self, ParseError> {
+        for (name, rule) in RENAME_RULES {
+            if rename_all_str == *name {
+                return Ok(*rule);
+            }
+        }
+        Err(ParseError {
+            unknown: rename_all_str,
+        })
+    }
+
+    /// Apply a renaming rule to a struct field, returning the version expected in the source.
+    pub fn apply_to_field(&self, field: &str) -> String {
+        match *self {
+            RenameRule::None | RenameRule::LowerCase | RenameRule::SnakeCase => field.to_owned(),
+            RenameRule::UpperCase => field.to_ascii_uppercase(),
+            RenameRule::PascalCase => {
+                let mut pascal = String::new();
+                let mut capitalize = true;
+                for ch in field.chars() {
+                    if ch == '_' {
+                        capitalize = true;
+                    } else if capitalize {
+                        pascal.push(ch.to_ascii_uppercase());
+                        capitalize = false;
+                    } else {
+                        pascal.push(ch);
+                    }
+                }
+                pascal
+            }
+            RenameRule::CamelCase => {
+                let pascal = RenameRule::PascalCase.apply_to_field(field);
+                pascal[..1].to_ascii_lowercase() + &pascal[1..]
+            }
+            RenameRule::ScreamingSnakeCase => field.to_ascii_uppercase(),
+            RenameRule::KebabCase => field.replace('_', "-"),
+            RenameRule::ScreamingKebabCase => RenameRule::ScreamingSnakeCase
+                .apply_to_field(field)
+                .replace('_', "-"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ParseError<'a> {
+    unknown: &'a str,
+}
+
+impl<'a> Display for ParseError<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("unknown rename rule `rename_all = ")?;
+        Debug::fmt(self.unknown, f)?;
+        f.write_str("`, expected one of ")?;
+        for (i, (name, _rule)) in RENAME_RULES.iter().enumerate() {
+            if i > 0 {
+                f.write_str(", ")?;
+            }
+            Debug::fmt(name, f)?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn rename_fields() {
+        for &(original, upper, pascal, camel, screaming, kebab, screaming_kebab) in &[
+            (
+                "outcome", "OUTCOME", "Outcome", "outcome", "OUTCOME", "outcome", "OUTCOME",
+            ),
+            (
+                "very_tasty",
+                "VERY_TASTY",
+                "VeryTasty",
+                "veryTasty",
+                "VERY_TASTY",
+                "very-tasty",
+                "VERY-TASTY",
+            ),
+            ("a", "A", "A", "a", "A", "a", "A"),
+            ("z42", "Z42", "Z42", "z42", "Z42", "z42", "Z42"),
+        ] {
+            assert_eq!(RenameRule::None.apply_to_field(original), original);
+            assert_eq!(RenameRule::UpperCase.apply_to_field(original), upper);
+            assert_eq!(RenameRule::PascalCase.apply_to_field(original), pascal);
+            assert_eq!(RenameRule::CamelCase.apply_to_field(original), camel);
+            assert_eq!(RenameRule::SnakeCase.apply_to_field(original), original);
+            assert_eq!(
+                RenameRule::ScreamingSnakeCase.apply_to_field(original),
+                screaming
+            );
+            assert_eq!(RenameRule::KebabCase.apply_to_field(original), kebab);
+            assert_eq!(
+                RenameRule::ScreamingKebabCase.apply_to_field(original),
+                screaming_kebab
+            );
+        }
+    }
+}
--- astarte-device-sdk-derive/src/lib.rs
+++ astarte-device-sdk-derive/src/lib.rs
@@ -18,9 +18,34 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+mod case;
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse_macro_input;
+use syn::Attribute;
+
+use case::RenameRule;
+
+#[proc_macro_attribute]
+pub fn astarte_aggregate(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast_attr = parse_macro_input!(attr as syn::AttributeArgs);
+    let ast_item = parse_macro_input!(item as syn::ItemStruct);
+
+    impl_astarte_aggregate_attr(ast_attr, ast_item)
+}
+
+fn impl_astarte_aggregate_attr(
+    _ast_attr: syn::AttributeArgs,
+    ast_item: syn::ItemStruct,
+) -> TokenStream {
+    // Do not perform any operation. All the checks and changes are performed by the derive
+    // macro.
+
+    TokenStream::from(quote!(#ast_item))
+}
 
 #[proc_macro_derive(AstarteAggregate)]
 pub fn astarte_aggregate_derive(input: TokenStream) -> TokenStream {
@@ -29,23 +54,31 @@ pub fn astarte_aggregate_derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
 
     // Build the trait implementation
-    impl_astarte_aggregate(ast)
+    impl_astarte_aggregate_derive(ast)
 }
 
-fn impl_astarte_aggregate(ast: syn::DeriveInput) -> TokenStream {
+fn impl_astarte_aggregate_derive(ast: syn::DeriveInput) -> TokenStream {
+    let mut rename_rule = RenameRule::None;
+    if let Some(astarte_aggregate_attr) =
+        find_astarte_aggregate_in_attributes_list(&ast.attrs).unwrap()
+    {
+        rename_rule = parse_astarte_aggregate_attribute(astarte_aggregate_attr).unwrap();
+    }
+
     if let syn::Data::Struct(st) = ast.data {
         if let syn::Fields::Named(fields) = st.fields {
-            let mut fields_names = (Vec::new(), Vec::new());
+            let mut fields_names_renamed = Vec::new();
+            let mut fields_names_ident = Vec::new();
             for field in fields.named {
                 let ident = field
                     .ident
                     .expect("AstarteAggregate is not implementable over this struct");
-                fields_names.0.push(ident.to_string());
-                fields_names.1.push(ident);
+                fields_names_renamed.push(rename_rule.apply_to_field(&ident.to_string()));
+                fields_names_ident.push(ident);
             }
             let name = &ast.ident;
-            let fields_names_str_iter = fields_names.0.iter();
-            let fields_names_iter = fields_names.1.iter();
+            let fields_names_renamed_iter = fields_names_renamed.iter();
+            let fields_names_ident_iter = fields_names_ident.iter();
             let gen = quote! {
                 impl AstarteAggregate for #name {
                     fn astarte_aggregate(
@@ -57,8 +90,8 @@ fn impl_astarte_aggregate(ast: syn::DeriveInput) -> TokenStream {
                         let mut result = std::collections::HashMap::new();
                         #(
                             let astype: astarte_device_sdk::types::AstarteType =
-                                std::convert::TryInto::try_into(self.#fields_names_iter)?;
-                            result.insert(#fields_names_str_iter.to_string(), astype);
+                                std::convert::TryInto::try_into(self.#fields_names_ident_iter)?;
+                            result.insert(#fields_names_renamed_iter.to_string(), astype);
                         )*
                         Ok(result)
                     }
@@ -71,4 +104,45 @@ fn impl_astarte_aggregate(ast: syn::DeriveInput) -> TokenStream {
     } else {
         panic!("AstarteAggregate is only implementable over a struct.")
     }
+}
+
+fn find_astarte_aggregate_in_attributes_list(
+    attrs: &[Attribute],
+) -> Result<Option<&Attribute>, String> {
+    let astarte_aggregate_list = attrs
+        .iter()
+        .filter(|e| {
+            if let Some(segment) = e.path.segments.last() {
+                if segment.ident == "astarte_aggregate" {
+                    return true;
+                }
+            }
+            false
+        })
+        .collect::<Vec<_>>();
+
+    if let [astarte_aggregate_attr] = astarte_aggregate_list[..] {
+        Ok(Some(astarte_aggregate_attr))
+    } else if astarte_aggregate_list.is_empty() {
+        Ok(None)
+    } else {
+        Err("Duplicated astarte_aggregate attribute.".to_string())
+    }
+}
+
+fn parse_astarte_aggregate_attribute(attr: &Attribute) -> Result<RenameRule, String> {
+    if let Ok(syn::Meta::List(meta_list)) = &attr.parse_meta() {
+        let meta_list_nested = &meta_list.nested;
+        if meta_list_nested.len() == 1 {
+            if let Some(syn::NestedMeta::Meta(syn::Meta::NameValue(meta_name_value))) =
+                meta_list_nested.first()
+            {
+                if let syn::Lit::Str(lit_str) = &meta_name_value.lit {
+                    return RenameRule::from_str(&lit_str.value())
+                        .map_err(|_| format!("Unrecognize syntax rule {}", lit_str.value()));
+                }
+            }
+        }
+    }
+    Err("Incorrectly formatted astarte_aggregate attribute.".to_string())
 }
