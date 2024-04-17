d11142a3d33df203d0f53e701b22418b5df0ddff
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-04-17 10:33:29 +0200

    feat(mock): expose mock dependency
    Signed-off-by: Joshua Chapman <joshua.chapman@joshuachp.dev>

--- astarte-device-sdk-mock/src/lib.rs
+++ astarte-device-sdk-mock/src/lib.rs
@@ -22,6 +22,9 @@ use astarte_device_sdk::{AstarteAggregate, AstarteType, DeviceEvent, Error, Inte
 use async_trait::async_trait;
 use mockall::mock;
 
+// Export public facing dependencies
+pub use mockall;
+
 #[async_trait]
 pub trait Client {
     async fn send_object_with_timestamp<D>(
