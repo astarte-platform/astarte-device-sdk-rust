d9413f39b0161f404280a0638849eda144116eed
Joshua Chapman <joshua.chapman@secomind.com>
2025-02-21 17:55:00 +0100

    chore(docs): add message hub client example
    Show how to connect a client to the MessageHub to send and receive
    data.
    
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

--- src/_docs/_connect_to_the_astarte_msghub.rs
+++ src/_docs/_connect_to_the_astarte_msghub.rs
@@ -6,7 +6,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,9 +16,5 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-//! Documentation and examples on how to use the library.
-
-#[cfg(feature = "message-hub")]
-#[cfg_attr(docsrs, doc(cfg(feature = "message-hub")))]
-pub mod _connect_to_the_astarte_msghub;
-pub mod _get_started;
+//! Example on how to connect to the Astarte MessageHub.
+#![doc = include_str!("../../docs/connect-to-the-astarte-message-hub.md")]
